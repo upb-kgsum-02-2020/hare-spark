9925d673c9c69b877930a80e7f91a28e3c34c2f5
Kohpai <whcwhc78@gmail.com>
2021-06-25 01:20:30 +0200

    :sparkles: filter dedicated logic

---
+++ src/main/scala/Filter.scala
@@ -0,0 +1,121 @@
+package io.kohpai
+
+import net.sansa_stack.rdf.spark.io._
+import org.apache.jena.graph.{Node, Triple}
+import org.apache.spark.mllib.linalg.distributed.MatrixEntry
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+
+object Filter {
+
+  val filterDir = "filter"
+  val entitiesDir = "entities"
+  val distDir = "dist"
+
+  var triples = ""
+  var nodes = ""
+  var predicates = ""
+  var nonPredicates = ""
+  var triplesSrc = ""
+  var entitiesSrc = ""
+  var s_n_dist = ""
+  var s_t_dist = ""
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession
+      .builder()
+      .appName(
+        "Filter-" + args(0).substring(args(0).lastIndexOf("/") + 1)
+      )
+      .getOrCreate()
+
+    triples = s"${args(0)}/$filterDir/triples"
+    nodes = s"${args(0)}/$filterDir/nodes"
+    predicates = s"${args(0)}/$filterDir/predicates"
+    nonPredicates = s"${args(0)}/$filterDir/non-predicates"
+    triplesSrc = s"${args(0)}/$entitiesDir/triples"
+    entitiesSrc = s"${args(0)}/$entitiesDir/nodes"
+    s_n_dist = s"${args(0)}/$distDir/s_n"
+    s_t_dist = s"${args(0)}/$distDir/s_t"
+
+    val mode = args(1)
+
+    val sc = spark.sparkContext
+
+    val triplesWithIndex: RDD[(Long, Triple)] = sc.objectFile(triplesSrc)
+    val nodesWithIndex: RDD[(Long, Node)] = sc.objectFile(entitiesSrc)
+    val dist: RDD[MatrixEntry] =
+      if (mode == "TRIPLES") sc.objectFile(s_t_dist)
+      else sc.objectFile(s_n_dist)
+
+    val entries = dist.map(f => (f.i, f.value))
+    val mean =
+      entries.map(_._2).reduce { case (a, b) => a + b } / entries.count()
+
+    if (mode == "TRIPLES") {
+      filterEntries(args(1), mean, entries, triplesWithIndex, triplesWithIndex)
+    } else {
+      filterEntries(args(1), mean, entries, nodesWithIndex, triplesWithIndex)
+    }
+
+    spark.stop()
+  }
+
+  def filterEntries[T](
+      mode: String,
+      mean: Double,
+      entries: RDD[(Long, Double)],
+      entriesWithIndex: RDD[(Long, T)],
+      triplesWithIndex: RDD[(Long, Triple)]
+  ): Unit = {
+    val entriesAboveMean = entries
+      .filter(_._2 > mean)
+      .join(entriesWithIndex)
+      .map(f => (f._2._2, f._2._1))
+
+    if (mode == "TRIPLES") {
+      entriesAboveMean
+        .map(_._1.asInstanceOf[Triple])
+        .saveAsNTriplesFile(triples)
+      return
+    }
+
+    val hashToNodePairs = entriesAboveMean.map(f =>
+      (f._1.asInstanceOf[Node].hashCode(), f._1.asInstanceOf[Node])
+    )
+    val nodeToTriplePairs = triplesWithIndex
+      .map(_._2)
+      .flatMap(f =>
+        mode match {
+          case "NODES" =>
+            Array(
+              (f.getSubject.hashCode, f),
+              (f.getPredicate.hashCode, f),
+              (f.getObject.hashCode, f)
+            )
+          case "PREDICATES" =>
+            Array(
+              (f.getPredicate.hashCode, f)
+            )
+          case "NON_PREDICATES" =>
+            Array(
+              (f.getSubject.hashCode, f),
+              (f.getObject.hashCode, f)
+            )
+          case _ => throw IllegalArgumentException
+        }
+      )
+
+    hashToNodePairs
+      .join(nodeToTriplePairs)
+      .map(_._2._2)
+      .distinct()
+      .saveAsNTriplesFile(mode match {
+        case "NODES"          => nodes
+        case "PREDICATES"     => predicates
+        case "NON_PREDICATES" => nonPredicates
+        case _                => throw IllegalArgumentException
+      })
+  }
+}
--- src/main/scala/Hare.scala
+++ src/main/scala/Hare.scala
@@ -2,62 +2,37 @@ package io.kohpai
 
 import utils.{DistanceUtils, MatrixUtils}
 
-import net.sansa_stack.rdf.spark.io._
-import org.apache.jena.graph.{Node, Triple}
 import org.apache.spark.mllib.linalg.distributed.{CoordinateMatrix, MatrixEntry}
-import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 import java.math.BigDecimal
-import scala.collection.mutable.ListBuffer
 
 object Hare {
 
   val df = 0.85
 
-  var w_path = "/matrices/w"
-  var f_path = "/matrices/f"
-  var s_n_destWithProbs = "/results_hare/s_n-with-probs"
-  var s_t_destWithProbs = "/results_hare/s_t-with-probs"
-  var s_t_dest = "/results_hare/triples/s_t"
-  var s_n_dest = "/results_hare/triples/s_n"
-  var s_t_dist = "/results_hare/dist/s_t"
-  var s_n_dist = "/results_hare/dist/s_n"
-  var statistics_dest = "/hare_statistics"
-  var triples_src = "/entities/triples"
-  var entities_src = "/entities/entities"
+  val matricesDir = "matrices"
+  val distDir = "dist"
 
   def main(args: Array[String]): Unit = {
 
     val spark = SparkSession
       .builder()
       .appName(
-        "HareScalaSpark-" + args(0).substring(args(0).lastIndexOf("/") + 1)
+        "HARE-" + args(0).substring(args(0).lastIndexOf("/") + 1)
       )
       .getOrCreate()
 
-    w_path = args(0) + w_path
-    f_path = args(0) + f_path
-    s_n_destWithProbs = args(0) + s_n_destWithProbs
-    s_t_destWithProbs = args(0) + s_t_destWithProbs
-    s_t_dest = args(0) + s_t_dest
-    s_n_dest = args(0) + s_n_dest
-    s_t_dist = args(0) + s_t_dist
-    s_n_dist = args(0) + s_n_dist
-    statistics_dest = args(0) + statistics_dest
-    triples_src = args(0) + triples_src
-    entities_src = args(0) + entities_src
+    val w_path = s"${args(0)}/$matricesDir/w"
+    val f_path = s"${args(0)}/$matricesDir/f"
+    val s_t_dist = s"${args(0)}/$distDir/s_t"
+    val s_n_dist = s"${args(0)}/$distDir/s_n"
 
     val sc = spark.sparkContext
 
     // W and F transition matrices for finding P(N) -> S(N) -> S(T) -> S
     val w = new CoordinateMatrix(sc.objectFile(w_path))
     val f = new CoordinateMatrix(sc.objectFile(f_path))
-
-    val t1 = System.currentTimeMillis()
-
-    val triplesWithIndex: RDD[(Long, Triple)] = sc.objectFile(triples_src)
-    val entitiesWithIndex: RDD[(Long, Node)] = sc.objectFile(entities_src)
 
     // transition matrix P(N)
     val p_n = MatrixUtils.coordinateMatrixMultiply(f, w)
@@ -82,16 +57,11 @@ object Hare {
       MatrixEntry(x, 0, 1)
     })
 
-    val matrixLoadTime = (System.currentTimeMillis() - t1) / 1000
-
     var s_t_final = s_n_final
     var s_n_previous = s_n_final
 
     val epsilon = new BigDecimal(0.001)
     var distance = new BigDecimal(1)
-
-    val t2 = System.currentTimeMillis()
-    var iter = 0
 
     val a = MatrixUtils.multiplyMatrixByNumber(p_n, df).transpose()
     val b = MatrixUtils.divideMatrixByNumber(
@@ -99,11 +69,8 @@ object Hare {
       s_n_v.toDouble
     )
 
-    val iter_list = new ListBuffer[Long]
-
+    var iter = 0
     while (distance.compareTo(epsilon) == 1 && iter < 1000) {
-      val time_iter_begin = System.currentTimeMillis()
-
       s_n_previous = s_n_final
 
       s_n_final = MatrixUtils.coordinateMatrixSum(
@@ -120,9 +87,6 @@ object Hare {
       )
 
       iter = iter + 1
-
-      iter_list += ((System.currentTimeMillis() - time_iter_begin) / 1000)
-
     }
 
     System.gc()
@@ -131,71 +95,6 @@ object Hare {
     s_n_final.entries.saveAsObjectFile(s_n_dist)
     s_t_final.entries.saveAsObjectFile(s_t_dist)
 
-    val (s_n_mean, s_n) = aboveMean[Node](
-      s_n_final.entries
-        .map(matrixEntryToTuple)
-        .join(entitiesWithIndex)
-        .map(extractAndSwitch[Node])
-    )
-
-    val nodes = s_n.map(f => (f._1.hashCode(), f._1))
-    val nodeTriples = triplesWithIndex
-      .map(_._2)
-      .flatMap(f =>
-        Array(
-          (f.getSubject.hashCode, f),
-          (f.getPredicate.hashCode, f),
-          (f.getObject.hashCode, f)
-        )
-      )
-
-    nodes.join(nodeTriples).map(_._2._2).distinct().saveAsNTriplesFile(s_n_dest)
-
-    s_n.saveAsTextFile(s_n_destWithProbs)
-
-    val (s_t_mean, s_t) = aboveMean[Triple](
-      s_t_final.entries
-        .map(matrixEntryToTuple)
-        .join(triplesWithIndex)
-        .map(extractAndSwitch[Triple])
-    )
-
-    s_t.saveAsTextFile(s_t_destWithProbs)
-    s_t.map(_._1).saveAsNTriplesFile(s_t_dest)
-
-    val hareTime = (System.currentTimeMillis() - t2) / 1000
-
-    val statistics = new ListBuffer[String]()
-    statistics += "Iterations: " + iter
-    statistics += "Iteration avg time: " + computeIterTimeMean(iter_list)
-    statistics += "Hare Computation Time: " + hareTime
-    statistics += "Matrices Load Time: " + matrixLoadTime
-    statistics += "Entities mean: " + s_n_mean
-    statistics += "Triples mean: " + s_t_mean
-
-    val rdd_statistics = sc.parallelize(statistics)
-    rdd_statistics.repartition(1).saveAsTextFile(statistics_dest)
-
     spark.stop()
   }
-
-  def computeIterTimeMean(list: ListBuffer[Long]): Double = {
-    var sum = 0L
-    list.foreach(sum += _)
-    sum.toDouble / list.size.toDouble
-  }
-
-  def aboveMean[T](rdd: RDD[(T, Double)]): (Double, RDD[(T, Double)]) = {
-    val mean = rdd.map(f => f._2).reduce { case (a, b) => a + b } / rdd.count()
-    (mean, rdd.filter(f => f._2 > mean))
-  }
-
-  def topScores(rdd: RDD[(String, Double)]): Array[(String, Double)] = {
-    rdd.sortBy(f => f._2, ascending = false).top(10000)
-  }
-
-  def matrixEntryToTuple(me: MatrixEntry): (Long, Double) = (me.i, me.value)
-  def extractAndSwitch[T](x: (Long, (Double, T))): (T, Double) =
-    (x._2._2, x._2._1)
-
 }
